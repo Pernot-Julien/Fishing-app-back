32da2017860312e88a2fcb47536efc4b08d902ac
Raphael-Aldaz <raphael.aldaz@gmail.com>
2022-11-21 14:33:34 +0100

    Testing connexion to the BDD

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-import {fs} from 'node:fs'
+const mysql = require('mysql')
 
-consolelog(fs)
+const db = mysql.createConnection({
+  host:"https://caracal.o2switch.net:2083/cpsess8026920482/3rdparty/phpMyAdmin/db_structure.php?server=1&db=okgb8219_ProjetPeche",
+  port:'3306',
+  user:"pecheur2",
+  password:"Y@sMG[STQ5ZA"
+});
+
+db.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
